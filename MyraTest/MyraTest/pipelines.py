bdf2a5f2849f3bcda0992555991667c44883e7af
Ubuntu <ubuntu@ip-172-31-31-120.us-east-2.compute.internal>
2020-12-13 20:40:07 +0000

    Correção - Coluna INDEX do PANDAS removida

--- MyraTest/MyraTest/pipelines.py
+++ MyraTest/MyraTest/pipelines.py
@@ -1,6 +1,5 @@
 from itemadapter import ItemAdapter
 import pandas as pd
-from pandas import DataFrame
 from datetime import datetime
 
 class MyratestPipeline:
@@ -15,14 +14,13 @@ class MyratestPipeline:
         'TextFile': [saved_as]
         }
         df = pd.DataFrame.from_dict(item_dictionary, orient='columns')
-        df.to_csv("data.csv",sep=';',encoding='utf-8-sig',mode='a',header=False)
-        #df = DataFrame(item_dictionary, columns= ['Author', 'Tags', 'Page', 'Rule', 'TextFile'])
+        df.to_csv("result.csv",sep=';',encoding='utf-8-sig',mode='a',header=False,index=False)
         return item
 
         
     def save_text(self,text):
         #Salva texto em .txt
-        file_name = 'crawl' + str(datetime.now()) + '.txt'
+        file_name = 'text-extracted' + str(datetime.now()) + '.txt'
         f = open(file_name, "w")
         f.write(text)
         f.close()
